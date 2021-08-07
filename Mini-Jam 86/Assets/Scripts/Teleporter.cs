3d107f3aa1126cff8a1608a4c32b4048cf84c036
David Henshaw <davidhenshaw2014@gmail.com>
2021-08-07 02:14:24 -0500

    Teleporter kinda works

---
+++ Mini-Jam 86/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace metakazz{
+    public class Teleporter : MonoBehaviour
+    {
+        [SerializeField] Transform toTeleport;
+        [SerializeField] Camera mainCamera;
+        public bool isHorizontal;
+
+        private void Awake()
+        {
+            if(!mainCamera)
+            {
+                mainCamera = Camera.main;
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            TeleportObject();
+        }
+
+        void TeleportObject()
+        {
+            var pos = toTeleport.position;
+            toTeleport.position = isHorizontal ? new Vector3(pos.x, -pos.y, pos.z) : new Vector3(-pos.x, pos.y, pos.z);
+            Debug.Log("Teleported object");
+        }
+    }
+}
