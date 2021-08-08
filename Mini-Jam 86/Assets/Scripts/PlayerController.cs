c062afefb3506119a63d0213907dd19ce94c6b40
David Henshaw <davidhenshaw2014@gmail.com>
2021-08-08 14:41:08 -0500

    Import assets

---
+++ Mini-Jam 86/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace metakazz{
+    public class PlayerController : MonoBehaviour
+    {
+        float moveSpeed = 10;
+
+        public void Move(Vector2 dir)
+        {
+            var movement = dir * moveSpeed * Time.deltaTime;
+
+            transform.Translate(movement.x, movement.y, 0);
+        }
+    }
+}
